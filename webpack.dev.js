4a1abcf3c14cc35722474f3433c4f5dd51625181
tatyna <tatyna.absatarova@axelkg.com>
2026-09-20 18:47:06 +0600

    feat: add webpack dev and prod configs with entry files

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+    open: true,
+    port: 3000,
+    hot: true,
+  },
+});
